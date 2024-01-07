711514ee59f0a653e89ac379f2b2925cd7d5ad07
produy <nguyentrinhduy2003@gmail.com>
2024-01-07 21:43:19 +0700

    push globalApplication

---
+++ src/app/src/main/java/com/example/cs300_dailyapple/Models/GlobalApplication.java
@@ -0,0 +1,25 @@
+package com.example.cs300_dailyapple.Models;
+
+import android.app.Application;
+
+public class GlobalApplication extends Application {
+    private User user;
+
+    String currentMealChoosing = "";
+
+    public String getCurrentMealChoosing() {
+        return currentMealChoosing;
+    }
+
+    public void setCurrentMealChoosing(String currentMealChoosing) {
+        this.currentMealChoosing = currentMealChoosing;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
